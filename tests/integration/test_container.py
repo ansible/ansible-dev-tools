a5d7413be61f79378b83523408424b10acfcd631
Bradley A. Thornton <bthornto@redhat.com>
2024-07-22 14:10:57 -0700

    Additional images tests

--- tests/integration/test_container.py
+++ tests/integration/test_container.py
@@ -7,9 +7,12 @@ from pathlib import Path
 
 import pytest
 
+from ansible_navigator.utils.packaged_data import ImageEntry
+
 from ansible_dev_tools.version_builder import PKGS
 
 from ..conftest import Infrastructure  # noqa: TID252
+from .conftest import ContainerTmux
 from .test_server_creator import test_collection_v1 as tst_collection_v1
 from .test_server_creator import test_error as tst_error
 from .test_server_creator import test_playbook_v1 as tst_playbook_v1
@@ -122,3 +125,71 @@ def test_playbook_v1_container(server_in_container_url: str, tmp_path: Path) -> 
         tmp_path: The temporary directory.
     """
     tst_playbook_v1(server_url=server_in_container_url, tmp_path=tmp_path)
+
+
+@pytest.mark.container()
+def test_nav_collections(container_tmux: ContainerTmux, tmp_path: Path) -> None:
+    """Test ansible-navigator collections.
+
+    Args:
+        container_tmux: A tmux session attached to the container.
+        tmp_path: The temporary directory
+    """
+    cmd = f"ansible-navigator collections --lf {tmp_path}/navigator.log"
+    stdout = container_tmux.send_and_wait(cmd=cmd, wait_for=":help help", timeout=10)
+    assert any("ansible.builtin" in line for line in stdout)
+    assert any("ansible.posix" in line for line in stdout)
+    cmd = ":0"
+    stdout = container_tmux.send_and_wait(cmd=cmd, wait_for=":help help")
+    assert any("add_host" in line for line in stdout)
+
+
+@pytest.mark.container()
+def test_nav_images(container_tmux: ContainerTmux, tmp_path: Path) -> None:
+    """Test ansible-navigator images.
+
+    Args:
+        container_tmux: A tmux session attached to the container.
+        tmp_path: The temporary directory
+    """
+    cmd = f"ansible-navigator images --lf {tmp_path}/navigator.log"
+    stdout = container_tmux.send_and_wait(cmd=cmd, wait_for=":help help", timeout=10)
+    nav_default = ImageEntry.DEFAULT_EE.get(app_name="ansible_navigator")
+    assert any(nav_default in line for line in stdout)
+
+
+def test_nav_playbook(container_tmux: ContainerTmux, tmp_path: Path) -> None:
+    """Test ansible-navigator run using a creator created playbook.
+
+    Args:
+        container_tmux: A tmux session attached to the container.
+        tmp_path: The temporary directory
+    """
+    cmd = f"ansible-creator init playbook test_ns.test_name {tmp_path}"
+    stdout = container_tmux.send_and_wait(cmd=cmd, wait_for="created", timeout=10)
+    output = f"Note: ansible project created at {tmp_path}"
+    assert any(output in line for line in stdout)
+    cmd = f"cd {tmp_path} && ansible-navigator run site.yml"
+    stdout = container_tmux.send_and_wait(cmd=cmd, wait_for="Successful", timeout=10)
+    assert stdout[-1].endswith("Successful")
+
+
+def test_nav_collection(container_tmux: ContainerTmux, tmp_path: Path) -> None:
+    """Test ansible-navigator run using a creator created collection.
+
+    Args:
+        container_tmux: A tmux session attached to the container.
+        tmp_path: The temporary directory
+    """
+    namespace = "test_ns"
+    name = "test_name"
+    collection_path = tmp_path / "collections" / "ansible_collections" / namespace / name
+    cmd = f"ansible-creator init collection test_ns.test_name {collection_path}"
+    stdout = container_tmux.send_and_wait(cmd=cmd, wait_for="created", timeout=10)
+    output = f"Note: collection {namespace}.{name} created"
+    assert any(output in line for line in stdout)
+    cmd = (
+        f"ANSIBLE_COLLECTIONS_PATH={tmp_path}/collections ansible-navigator collections --ee false"
+    )
+    stdout = container_tmux.send_and_wait(cmd=cmd, wait_for=":help help", timeout=10)
+    assert any(f"{namespace}.{name}" in line for line in stdout)
