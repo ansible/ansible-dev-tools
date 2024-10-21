b2bdd76c1ec9c9c5fa4628fe03a9648841682f7c
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2024-10-21 12:24:08 +0100

    Further optimize the image building to reduce layers (#432)

--- final/setup.sh
+++ final/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# cspell: ignore onigurumacffi,makecache,euxo,libssh,overlayfs,setcaps,minrate,openh264
+# cspell: ignore onigurumacffi,makecache,euxo,libssh,overlayfs,setcaps,minrate,openh264,additionalimage,mountopt,nodev,iname,chsh
 set -euxo pipefail
 
 # When building for multiple-architectures in parallel using emulation
@@ -47,6 +47,85 @@ python3-pip \
 python3-pyyaml \
 python3-ruamel-yaml \
 python3-wheel \
---exclude container-selinux \
-    && microdnf -q clean all \
-    && ln -s /usr/bin/vim /usr/bin/vi
+--exclude container-selinux
+
+microdnf -q clean all
+ln -s /usr/bin/vim /usr/bin/vi
+
+curl -o /etc/containers/containers.conf https://raw.githubusercontent.com/containers/image_build/main/podman/containers.conf
+chmod 644 /etc/containers/containers.conf
+
+# Copy & modify the defaults to provide reference if runtime changes needed.
+# Changes here are required for running with fuse-overlay storage inside container.
+sed -e 's|^#mount_program|mount_program|g' \
+    -e '/additionalimage.*/a "/var/lib/shared",' \
+    -e 's|^mountopt[[:space:]]*=.*$|mountopt = "nodev,fsync=0"|g' \
+    /usr/share/containers/storage.conf \
+    > /etc/containers/storage.conf
+
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p /var/lib/shared/overlay-images /var/lib/shared/overlay-layers /var/lib/shared/vfs-images /var/lib/shared/vfs-layers
+touch /var/lib/shared/overlay-images/images.lock /var/lib/shared/overlay-layers/layers.lock /var/lib/shared/vfs-images/images.lock /var/lib/shared/vfs-layers/layers.lock
+
+# In OpenShift, container will run as a random uid number and gid 0. Make sure things
+# are writeable by the root group.
+for dir in \
+    /tmp/dist \
+    /home/runner \
+    /home/runner/.ansible \
+    /home/runner/.ansible/tmp \
+    /runner \
+    /home/runner \
+    /runner/env \
+    /runner/inventory \
+    /runner/project \
+    /runner/artifacts ; \
+    do
+    # shellcheck disable=SC2174
+    mkdir -m 0775 -p $dir
+    # do not use recursive (-R) because it will fail with read-only bind mounts
+    find $dir -type d -exec chmod g+rwx {} \;
+    find $dir -type f -exec chmod g+rw {} \;
+    find $dir -exec chgrp root {} \;
+done
+
+for file in /home/runner/.ansible/galaxy_token /etc/passwd /etc/group; do
+    touch $file
+    chmod g+rw $file
+    chgrp root $file;
+done
+
+# this must run as user root
+find "$DIR/dist/" -iname '*.whl' -maxdepth 1 -exec python3 -m pip install --no-cache-dir '{}[server,lock]' \;
+
+mkdir -p ~/.ansible/roles /usr/share/ansible/roles /etc/ansible/roles
+git config --system --add safe.directory /
+
+# The dev container for docker runs as root
+chsh -s "$(which zsh)" root
+
+# Install argcomplete
+python3 -m pip install argcomplete
+activate-global-python-argcomplete
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# add some helpful CLI commands to check we do not remove them inadvertently and output some helpful version information at build time.
+set -ex
+ansible --version
+ansible-lint --version
+ansible-runner --version
+molecule --version
+molecule drivers
+podman --version
+python3 --version
+git --version
+ansible-galaxy role list
+ansible-galaxy collection list
+rpm -qa
+uname -a
+
+# Make a workdir usable by the root user
+mkdir -p /workdir
